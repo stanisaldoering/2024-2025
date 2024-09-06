f4faec40f70ce9fc0be3730aa60668c9f48543f1
python 2024/2025 <stasiudoeringw@gmail.com>
2024-09-06 10:25:57 +0200

    test

---
+++ Wrzesien/06-09-2024.py
@@ -0,0 +1,8 @@
+# napisz funkcje zwracajaca 5 wyolosowychn liczb w liscie
+from random import randint
+def los(n):
+    lista=[]
+    for i in range(n):
+        lista.append(randint(0,100))
+    return lista
+print(los(5))
