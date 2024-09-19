8a395646af1a524feb7823224c091379b6905cac
python 2024/2025 <stasiudoeringw@gmail.com>
2024-09-19 12:26:06 +0200

    do domu i palindorm

---
+++ Wrzesien/19-09-2024/z1.py
@@ -0,0 +1,16 @@
+# wykorystanie list do rozwiazania problemu
+from random import randint
+lista=[randint(10000,999999999999999) for i in range(1000)]
+print(lista)
+
+# 1.liczby palindormiczne w nowej lisicie
+# 2. Liczby zawierające tylko liczby parzyste
+# 3. Zawierające liczby nieparzyste
+# 4 Liczba wesola
+p=[]
+def palindrom(l):
+    l=str(l)
+    for i in range(len(l)//2:
+        if l[i]!=l[len(l)-i-1]:
+        return Fasle
+return true
