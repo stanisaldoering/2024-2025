8d3f3ed274c6c92b555c7d66b3b633e8a5879238
python 2024/2025 <stasiudoeringw@gmail.com>
2024-10-11 11:28:25 +0200

    temperatura

--- Październik/10-10-2024/z1.py
+++ Październik/10-10-2024/z1.py
@@ -2,11 +2,46 @@ def pobieranie():
     temperatura={}
     with open ("temperatura.txt", 'r') as file:
         for line in file:
-            temperatura.update({line[0:10]:int(line[11::].strip())})
+            tmp=int(line[11::].strip())
+            temperatura.update({line[0:10]: tmp})
     return temperatura
-print(pobieranie())
 
 # podaj maskymalna dlugosc okresu wzrastania temperatury w podanym czasie
 
-for i in temperatura:
-    if i[0]
+dane=pobieranie()
+print(dane)
+tmplist=[]
+t=[]
+for i in dane.values():
+    tmplist.append(i)
+
+iloscdni=0
+tmpIloscdni=1
+for i in range(len(tmplist)-1):
+    if tmplist[i]<= tmplist[i+1]:
+        tmpIloscdni+=1
+    else:
+        if iloscdni < tmpIloscdni:
+            iloscdni=tmpIloscdni
+        tmpIloscdni=1
+print(iloscdni)
+
+# ile bylo dni w ktorych temperatura powietrza byla mniejsza niz srednia temperatura w calym okresie
+srednia=sum(tmplist)/ len(tmplist)
+print(srednia)
+t=[]
+for i in tmplist:
+    if i<srednia:
+        t.append(i)
+print(len(t))
+
+# oblicz srednie temepratury w poszczegolnych dniach tygdinia
+pn=[]
+wt=[]
+sr=[]
+czw=[]
+pt=[]
+sb=[]
+ndz=[]
+
+pt.append(tmplist[0]+7)
